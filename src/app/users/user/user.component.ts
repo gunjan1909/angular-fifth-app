47572773f8bdaec2de7b83f7f87c2cef61e72a3a
gunjan1909 <bhanarkaranita@gmail.com>
2023-08-28 00:00:25 +0530

    130

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-  user!: { id: number; name: string };
+  @Input() user!: { id: number; name: string };
 
   constructor() {}
 
